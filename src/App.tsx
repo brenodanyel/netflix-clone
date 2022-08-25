1dc39655e6165b26e011508fdca5842a66ed9414
brenodanyel <bdls1704@hotmail.com>
2022-08-24 21:06:16 -0300

    feat: activate featured movie component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Header } from './components/Header';
 import { CategoryList } from './components/CategoryList';
 import { Footer } from './components/Footer';
+import { FeaturedMovie } from './components/FeaturedMovie';
 
 export function App() {
   return (
     <>
       <Header />
+      <FeaturedMovie />
       <CategoryList />
       <Footer />
     </>
