940581e4b00949ffafb0f127a46019410f64a990
brenodanyel <bdls1704@hotmail.com>
2022-08-24 21:00:11 -0300

    refactor(header): remove scroll effect

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,29 +6,8 @@ import Profile from '../../assets/profile.png';
 import style from './header.module.scss';
 
 export function Header() {
-  const [hasBackground, setHasBackground] = React.useState(false);
-
-  React.useEffect(() => {
-    const handleScroll = () => {
-      setHasBackground(window.scrollY > 50);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
   return (
-    <header
-      className={
-        [
-          style.header,
-          hasBackground ? style.black : '',
-        ].join(' ')
-      }
-    >
+    <header className={style.header}>
       <a href="/">
         <img src={Netflix} alt="logo da netflix" className={style.netflix_logo} />
       </a>
