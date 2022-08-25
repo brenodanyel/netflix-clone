5f414ee81dc46df0ee131645b50cee59072cce42
brenodanyel <bdls1704@hotmail.com>
2022-08-24 21:02:15 -0300

    add mocks

---
+++ src/services/mock.ts
@@ -0,0 +1,557 @@
+export const movie = {
+  "adult": false,
+  "backdrop_path": "/56v2KjBlU4XaOv9rVYEQypROD7P.jpg",
+  "created_by": [
+    {
+      "id": 1179419,
+      "credit_id": "57599b0e925141378a002c87",
+      "name": "Matt Duffer",
+      "gender": 2,
+      "profile_path": "/kXO5CnSxC0znMAICGxnPeuGP73U.jpg"
+    },
+    {
+      "id": 1179422,
+      "credit_id": "57599b039251410a99001cce",
+      "name": "Ross Duffer",
+      "gender": 2,
+      "profile_path": "/kN1HdFViQkcJOQlNcvvFJIx9Uju.jpg"
+    }
+  ],
+  "episode_run_time": [
+    50
+  ],
+  "first_air_date": "2016-07-15",
+  "genres": [
+    {
+      "id": 18,
+      "name": "Drama"
+    },
+    {
+      "id": 10765,
+      "name": "Sci-Fi & Fantasy"
+    },
+    {
+      "id": 9648,
+      "name": "Mistério"
+    }
+  ],
+  "homepage": "https://www.netflix.com/title/80057281",
+  "id": 66732,
+  "in_production": true,
+  "languages": [
+    "en"
+  ],
+  "last_air_date": "2022-07-01",
+  "last_episode_to_air": {
+    "air_date": "2022-07-01",
+    "episode_number": 9,
+    "id": 3325045,
+    "name": "Capítulo 9: O Plano de Onze",
+    "overview": "Com muito amor no coração e um toque de metal, os heróis lutam para salvar Hawkins e o mundo.",
+    "production_code": "",
+    "runtime": 142,
+    "season_number": 4,
+    "show_id": 66732,
+    "still_path": "/fvoa0Hosu4yK7TUiHglV8TvjMUB.jpg",
+    "vote_average": 8.522,
+    "vote_count": 69
+  },
+  "name": "Stranger Things",
+  "next_episode_to_air": null,
+  "networks": [
+    {
+      "id": 213,
+      "name": "Netflix",
+      "logo_path": "/wwemzKWzjKYJFfCeiB57q3r4Bcm.png",
+      "origin_country": ""
+    }
+  ],
+  "number_of_episodes": 34,
+  "number_of_seasons": 4,
+  "origin_country": [
+    "US"
+  ],
+  "original_language": "en",
+  "original_name": "Stranger Things",
+  "overview": "Quando um garoto desaparece, a cidade toda participa nas buscas. Mas o que encontram são segredos, forças sobrenaturais e uma menina.",
+  "popularity": 1274.104,
+  "poster_path": "/49WJfeN0moxb9IPfGn8AIqMGskD.jpg",
+  "production_companies": [
+    {
+      "id": 2575,
+      "logo_path": "/9YJrHYlcfHtwtulkFMAies3aFEl.png",
+      "name": "21 Laps Entertainment",
+      "origin_country": "US"
+    },
+    {
+      "id": 170090,
+      "logo_path": null,
+      "name": "Monkey Massacre Productions",
+      "origin_country": "US"
+    }
+  ],
+  "production_countries": [
+    {
+      "iso_3166_1": "US",
+      "name": "United States of America"
+    }
+  ],
+  "seasons": [
+    {
+      "air_date": "2016-07-15",
+      "episode_count": 8,
+      "id": 77680,
+      "name": "1ª Temporada",
+      "overview": "A pequena cidade de Hawkins é cenário de estranhos acontecimentos. Após o desaparecimento de um garoto, surge uma menina com poderes sobrenaturais.",
+      "poster_path": "/rbnuP7hlynAMLdqcQRCpZW9qDkV.jpg",
+      "season_number": 1
+    },
+    {
+      "air_date": "2017-10-27",
+      "episode_count": 9,
+      "id": 83248,
+      "name": "2ª Temporada",
+      "overview": "Quase um ano após o estranho desaparecimento de Will, a vida em Hawkins está longe de voltar ao normal.",
+      "poster_path": "/lXS60geme1LlEob5Wgvj3KilClA.jpg",
+      "season_number": 2
+    },
+    {
+      "air_date": "2019-07-04",
+      "episode_count": 8,
+      "id": 115216,
+      "name": "3ª Temporada",
+      "overview": "Romance no ar. Um shopping novinho em folha. Ratazanas enlouquecidas correndo para o perigo. É o verão de 1985 em Hawkins. E um verão pode mudar tudo.",
+      "poster_path": "/x2LSRK2Cm7MZhjluni1msVJ3wDF.jpg",
+      "season_number": 3
+    },
+    {
+      "air_date": "2022-05-27",
+      "episode_count": 9,
+      "id": 163313,
+      "name": "4ª Temporada",
+      "overview": "",
+      "poster_path": "/49WJfeN0moxb9IPfGn8AIqMGskD.jpg",
+      "season_number": 4
+    }
+  ],
+  "spoken_languages": [
+    {
+      "english_name": "English",
+      "iso_639_1": "en",
+      "name": "English"
+    }
+  ],
+  "status": "Returning Series",
+  "tagline": "Isso só fica mais estranho...",
+  "type": "Scripted",
+  "vote_average": 8.637,
+  "vote_count": 13080
+};
+
+export const movies = {
+  "page": 1,
+  "results": [
+    {
+      "backdrop_path": "/9GvhICFMiRQA82vS6ydkXxeEkrd.jpg",
+      "first_air_date": "2022-08-18",
+      "genre_ids": [
+        35,
+        10759,
+        10765
+      ],
+      "id": 92783,
+      "name": "Mulher-Hulk: Defensora de Heróis",
+      "origin_country": [
+        "US"
+      ],
+      "original_language": "en",
+      "original_name": "She-Hulk: Attorney at Law",
+      "overview": "Jennifer Walters — uma advogada especializada em casos envolvendo super-humanos — deve lidar com a complicada vida de uma mulher solteira de trinta e poucos anos que, por acaso, é uma hulk superpoderosa de 2 metros.",
+      "popularity": 5548.946,
+      "poster_path": "/4xNH5hHPpTJptSWANyAX3OLZoNM.jpg",
+      "vote_average": 7.4,
+      "vote_count": 289
+    },
+    {
+      "backdrop_path": "/eqhKMZTLcieAvoH6CBqknTTfNby.jpg",
+      "first_air_date": "2022-08-05",
+      "genre_ids": [
+        10765,
+        18
+      ],
+      "id": 90802,
+      "name": "Sandman",
+      "origin_country": [
+        "US"
+      ],
+      "original_language": "en",
+      "original_name": "The Sandman",
+      "overview": "Um mago tenta capturar a Morte para barganhar pela vida eterna, no entanto, acaba prendendo seu irmão mais novo, Sonho. Temendo por sua segurança, o mago o mantém preso em uma garrafa de vidro por décadas. Após sua fuga, Sonho, também conhecido como Morpheus, parte em busca de seus poderosos objetos perdidos.",
+      "popularity": 2393.502,
+      "poster_path": "/q54qEgagGOYCq5D1903eBVMNkbo.jpg",
+      "vote_average": 8.2,
+      "vote_count": 494
+    },
+    {
+      "backdrop_path": "/Aa9TLpNpBMyRkD8sPJ7ACKLjt0l.jpg",
+      "first_air_date": "2022-08-21",
+      "genre_ids": [
+        18,
+        10759,
+        10765
+      ],
+      "id": 94997,
+      "name": "A Casa do Dragão",
+      "origin_country": [
+        "US"
+      ],
+      "original_language": "en",
+      "original_name": "House of the Dragon",
+      "overview": "200 anos antes dos eventos de \"Game of Thrones\", os Targaryen estavam no ápice de seu poder, tendo inúmeros dragões sob seu comando, mas nem tudo dura para sempre. O início do fim da dinastia Targaryen.",
+      "popularity": 2555.349,
+      "poster_path": "/ruHPkmmAwBTCzNXtHot18rs6ctN.jpg",
+      "vote_average": 8.9,
+      "vote_count": 320
+    },
+    {
+      "backdrop_path": "/xlKKD1TXJvh0YYlVPqqQ3g3ZUjM.jpg",
+      "first_air_date": "2018-07-01",
+      "genre_ids": [
+        16,
+        10759,
+        10765
+      ],
+      "id": 80020,
+      "name": "Dragon Ball Heroes",
+      "origin_country": [
+        "JP"
+      ],
+      "original_language": "ja",
+      "original_name": "スーパードラゴンボールヒーローズ",
+      "overview": "Trunks retorna do futuro para treinar com Goku e Vegeta. No entanto, desaparece sem aviso prévio. Então o misterioso Fu aparece, dizendo que Trunks foi aprisionado no Planeta da Prisão, um complexo misterioso em um lugar desconhecido nos universos. O grupo procura das esferas do dragão para libertar Trunks, mas uma batalha sem fim espera por eles! Goku e os outros vão resgatar Trunks e escapar do Planeta da Prisão?",
+      "popularity": 1533.545,
+      "poster_path": "/8jq6xv5c1WK7KAPOXCsodm8eUxp.jpg",
+      "vote_average": 7.6,
+      "vote_count": 2692
+    },
+    {
+      "backdrop_path": "/56v2KjBlU4XaOv9rVYEQypROD7P.jpg",
+      "first_air_date": "2016-07-15",
+      "genre_ids": [
+        18,
+        10765,
+        9648
+      ],
+      "id": 66732,
+      "name": "Stranger Things",
+      "origin_country": [
+        "US"
+      ],
+      "original_language": "en",
+      "original_name": "Stranger Things",
+      "overview": "Quando um garoto desaparece, a cidade toda participa nas buscas. Mas o que encontram são segredos, forças sobrenaturais e uma menina.",
+      "popularity": 1274.104,
+      "poster_path": "/r2w5UNf2mO2Mdl4q6HopuBms6XM.jpg",
+      "vote_average": 8.6,
+      "vote_count": 13065
+    },
+    {
+      "backdrop_path": "/iSA7iVTzExSuXvjDEoFELwY5JgT.jpg",
+      "first_air_date": "2019-07-09",
+      "genre_ids": [
+        10764
+      ],
+      "id": 90521,
+      "name": "Ilha do Amor",
+      "origin_country": [
+        "US"
+      ],
+      "original_language": "en",
+      "original_name": "Love Island",
+      "overview": "Um grupo de jovens solteiros sarados que vieram em busca de um verão de amor e romance em Fiji. O casal vencedor sairá da ilha com um grande prêmio em dinheiro.",
+      "popularity": 1268.271,
+      "poster_path": "/wmoHdVNDtFCUd6tmBJ7gf4itqIK.jpg",
+      "vote_average": 7,
+      "vote_count": 53
+    },
+    {
+      "backdrop_path": "/suopoADq0k8YZr4dQXcU6pToj6s.jpg",
+      "first_air_date": "2011-04-17",
+      "genre_ids": [
+        10765,
+        18,
+        10759
+      ],
+      "id": 1399,
+      "name": "Game of Thrones",
+      "origin_country": [
+        "US"
+      ],
+      "original_language": "en",
+      "original_name": "Game of Thrones",
+      "overview": "Em uma terra onde os verões podem durar vários anos e o inverno toda uma vida, sete nobres famílias lutam pelo controle da mítica terra de Westeros, dividida depois de uma guerra. Num cenário que lembra a Europa medieval, reis, rainhas, cavaleiros e renegados usam todos os meios possíveis em um jogo político pela disputa do Trono de Ferro, o símbolo do poder absoluto.",
+      "popularity": 1188.797,
+      "poster_path": "/l2ezB41chGDjXcKo24lseuXYtKP.jpg",
+      "vote_average": 8.4,
+      "vote_count": 18566
+    },
+    {
+      "backdrop_path": "/uFU4ItwbxHnFHxntb76ASnZbWTN.jpg",
+      "first_air_date": "2022-08-14",
+      "genre_ids": [
+        18,
+        10759,
+        10765
+      ],
+      "id": 136248,
+      "name": "Tales of the Walking Dead",
+      "origin_country": [
+        "US"
+      ],
+      "original_language": "en",
+      "original_name": "Tales of the Walking Dead",
+      "overview": "Série Antológica composta de episódios independentes focados em personagens novos e já apresentados do universo The walking dead",
+      "popularity": 1006.56,
+      "poster_path": "/zRMUHvTgQ79zteQafNI46Nd9XFm.jpg",
+      "vote_average": 7.5,
+      "vote_count": 21
+    },
+    {
+      "backdrop_path": "/jW61BMd1ZVWPXI7Ts4iC2BTx8Qj.jpg",
+      "first_air_date": "2022-04-11",
+      "genre_ids": [
+        18
+      ],
+      "id": 196572,
+      "name": "비밀의 집",
+      "origin_country": [
+        "KR"
+      ],
+      "original_language": "ko",
+      "original_name": "비밀의 집",
+      "overview": "",
+      "popularity": 1286.758,
+      "poster_path": "/wNN9hJDV45BF2PiJQQVwBCiqLIl.jpg",
+      "vote_average": 6.5,
+      "vote_count": 4
+    },
+    {
+      "backdrop_path": "/kOkmTrD8kWLeTXcAEwctg6GwW7t.jpg",
+      "first_air_date": "2017-02-20",
+      "genre_ids": [
+        10751,
+        35
+      ],
+      "id": 91759,
+      "name": "愛．回家之開心速遞",
+      "origin_country": [
+        "HK"
+      ],
+      "original_language": "cn",
+      "original_name": "愛．回家之開心速遞",
+      "overview": "",
+      "popularity": 1125.619,
+      "poster_path": "/5hYejOX8mkPuppAjJy6E37EoD6H.jpg",
+      "vote_average": 4.7,
+      "vote_count": 6
+    },
+    {
+      "backdrop_path": "/x0RRnWdYeczF4KXDqW8blda7SKS.jpg",
+      "first_air_date": "2022-03-28",
+      "genre_ids": [
+        18,
+        10766
+      ],
+      "id": 158415,
+      "name": "Pantanal",
+      "origin_country": [
+        "BR"
+      ],
+      "original_language": "pt",
+      "original_name": "Pantanal",
+      "overview": "Uma das novelas mais presentes no imaginário brasileiro, \"Pantanal\" é uma saga familiar sobre os Leôncio e os Marruá, que tem como cenário um dos mais ricos ecossistemas do mundo.",
+      "popularity": 1038.595,
+      "poster_path": "/wRzDupqEtggcrs9032yNgGNxDA.jpg",
+      "vote_average": 5.7,
+      "vote_count": 41
+    },
+    {
+      "backdrop_path": "/ry9SsBPOShH7eEL2dJc1coCBh8X.jpg",
+      "first_air_date": "2022-03-07",
+      "genre_ids": [
+        35,
+        18
+      ],
+      "id": 157936,
+      "name": "Por Ti",
+      "origin_country": [
+        "PT"
+      ],
+      "original_language": "pt",
+      "original_name": "Por Ti",
+      "overview": "",
+      "popularity": 948.494,
+      "poster_path": "/7GmlOYoR2GNiBRFs47azHIcqncR.jpg",
+      "vote_average": 4.9,
+      "vote_count": 8
+    },
+    {
+      "backdrop_path": "/3DLeOz8h2EScua1U0kPz0XPi4W2.jpg",
+      "first_air_date": "2019-07-25",
+      "genre_ids": [
+        10765,
+        10759
+      ],
+      "id": 76479,
+      "name": "The Boys",
+      "origin_country": [
+        "US"
+      ],
+      "original_language": "en",
+      "original_name": "The Boys",
+      "overview": "Na trama, conhecemos um mundo em que super-heróis são as maiores celebridades do planeta, e rotineiramente abusam dos seus poderes ao invés de os usarem para o bem.",
+      "popularity": 979.286,
+      "poster_path": "/mY7SeH4HFFxW1hiI6cWuwCRKptN.jpg",
+      "vote_average": 8.5,
+      "vote_count": 7083
+    },
+    {
+      "backdrop_path": "/wiE9doxiLwq3WCGamDIOb2PqBqc.jpg",
+      "first_air_date": "2013-09-12",
+      "genre_ids": [
+        18,
+        80
+      ],
+      "id": 60574,
+      "name": "Peaky Blinders: Sangue, Apostas e Navalhas",
+      "origin_country": [
+        "GB"
+      ],
+      "original_language": "en",
+      "original_name": "Peaky Blinders",
+      "overview": "Thomas Shelby e seus irmãos retornam a Birmingham depois de servir no exército britânico durante a Primeira Guerra Mundial. Os Peaky Blinders, a gangue na qual Thomas é líder, controlam a cidade de Birmingham. Mas, como as ambições de Shelby se estendem para além de Birmingham, ele planeja construir o império de negócios que criou e impedir qualquer um que atrapalhar.",
+      "popularity": 876.831,
+      "poster_path": "/i0uajcHH9yogXMfDHpOXexIukG9.jpg",
+      "vote_average": 8.6,
+      "vote_count": 7137
+    },
+    {
+      "backdrop_path": "/xZUZ9i6vVayjyhR1vRo9Bjku4h.jpg",
+      "first_air_date": "2016-01-25",
+      "genre_ids": [
+        80,
+        10765
+      ],
+      "id": 63174,
+      "name": "Lucifer",
+      "origin_country": [
+        "US"
+      ],
+      "original_language": "en",
+      "original_name": "Lucifer",
+      "overview": "Entediado com a vida nas trevas, o diabo se muda para Los Angeles, abre um piano-bar e empresta sua sabedoria a uma investigadora de assassinatos.",
+      "popularity": 860.88,
+      "poster_path": "/hdKxcoV5CFc3sGOmbGXDXbx1cTZ.jpg",
+      "vote_average": 8.5,
+      "vote_count": 12271
+    },
+    {
+      "backdrop_path": "/vNfuoO1JU4cfE4aJjLib2LsKYRb.jpg",
+      "first_air_date": "2021-07-12",
+      "genre_ids": [
+        10764
+      ],
+      "id": 124428,
+      "name": "B&B Vol Liefde",
+      "origin_country": [
+        "NL"
+      ],
+      "original_language": "nl",
+      "original_name": "B&B Vol Liefde",
+      "overview": "",
+      "popularity": 812.953,
+      "poster_path": "/oW1TpjHFYCoCMAwE3ZmrlhEVAT5.jpg",
+      "vote_average": 5,
+      "vote_count": 4
+    },
+    {
+      "backdrop_path": "/hNwopoqRDazw1uPHY2jSNmV2gS2.jpg",
+      "first_air_date": "2005-06-06",
+      "genre_ids": [
+        10763,
+        10764,
+        99
+      ],
+      "id": 203599,
+      "name": "東張西望",
+      "origin_country": [
+        "HK"
+      ],
+      "original_language": "cn",
+      "original_name": "東張西望",
+      "overview": "",
+      "popularity": 778.119,
+      "poster_path": "/qo6y0XvSBlKM3XCbYVdcR3a6qyQ.jpg",
+      "vote_average": 7.3,
+      "vote_count": 6
+    },
+    {
+      "backdrop_path": "/ic7YfRS52N3m4oalxoOIT665C33.jpg",
+      "first_air_date": "2020-07-08",
+      "genre_ids": [
+        18,
+        35
+      ],
+      "id": 104877,
+      "name": "Será Isso Amor?",
+      "origin_country": [
+        "TR"
+      ],
+      "original_language": "tr",
+      "original_name": "Sen Çal Kapımı",
+      "overview": "Eda, que liga todas as suas esperanças à sua educação, confronta Serkan Bolat, que corta sua bolsa de estudos internacional e a deixa com o diploma do ensino médio. Serkan Bolat oferece a Eda para devolver sua bolsa de estudos se ela fingir ser sua noiva por dois meses. Embora Eda rejeite a oferta deste homem porque ela o odeia, ela tem que aceitar quando as condições mudam. Enquanto fingem estar noivos, Serkan e Eda começam a experimentar um relacionamento apaixonado e desafiador que os fará esquecer tudo o que sabem direito. Porque o amor é difícil. E é por isso que é incrível.",
+      "popularity": 813.749,
+      "poster_path": "/wzdQ2gAdd2yq0RXO7xTVDMlssNR.jpg",
+      "vote_average": 8.2,
+      "vote_count": 2579
+    },
+    {
+      "backdrop_path": "/fMvRiykSZrb5KCQnHxe3dNN5XKX.jpg",
+      "first_air_date": "2022-06-27",
+      "genre_ids": [
+        18
+      ],
+      "id": 194619,
+      "name": "A Family Affair",
+      "origin_country": [
+        "PH"
+      ],
+      "original_language": "tl",
+      "original_name": "A Family Affair",
+      "overview": "",
+      "popularity": 785.244,
+      "poster_path": "/gkap4pWN722E0UK9WNL2vjGnAtw.jpg",
+      "vote_average": 5,
+      "vote_count": 2
+    },
+    {
+      "backdrop_path": "/vAMnMp84ASHuiHM6YxNa3esG1o9.jpg",
+      "first_air_date": "2020-08-17",
+      "genre_ids": [],
+      "id": 205401,
+      "name": "Ik Weet Er Alles Van! (incl) VIPS",
+      "origin_country": [
+        "NL"
+      ],
+      "original_language": "nl",
+      "original_name": "Ik Weet Er Alles Van! (incl) VIPS",
+      "overview": "",
+      "popularity": 771.16,
+      "poster_path": "/1UWnPL8nhyc6enkTjfJKM5Fcn0x.jpg",
+      "vote_average": 4.5,
+      "vote_count": 2
+    }
+  ],
+  "total_pages": 6776,
+  "total_results": 135503
+};
